b53dfa56b0dc955fa4794d675f66d2e0ec722754
Michael E <mickyfin10@hotmail.com>
2019-09-12 11:06:44 +0100

    started app

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  let name = "Max";
+  let age = 30;
+
+  $: uppercaseName = name.toUpperCase();
+
+  function incrementAge() {
+    age += 1;
+  }
+
+  function changeName() {
+    name = "Maximilian";
+  }
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<button on:click={incrementAge}>Change Age</button>
+<button on:click={changeName}>Change Name</button>
